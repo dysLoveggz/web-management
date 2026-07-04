e4b8462fd3f7f0bc9d159763b0adf7ff7118c77c
dysggz <dysggz@love.com>
2026-07-04 14:22:45 +0800

    main commit 1

--- web-management/src/main/java/com/dys/Git/gitTest.java
+++ web-management/src/main/java/com/dys/Git/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest {
         System.out.println("hello git!");
         System.out.println("hello dys!");
         System.out.println("hello zln!");
+        System.out.println("hello GGZ!");
     }
 }
